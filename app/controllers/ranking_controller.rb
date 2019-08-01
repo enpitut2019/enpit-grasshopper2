c40d7628ed446a008334a6ab9867fa4e834b9600
hiroto7 <hiroto450@gmail.com>
2019-08-01 12:55:32 +0900

    ランキングの表を表示

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,4 +1,5 @@
 class RankingController < ApplicationController
   def show
+    @users = [ { id: 1, name: '太郎', icon: 'a.png' }, { id: 2, name: '次郎', icon: 'b.png' }, { id: 3, name: '三郎', icon: 'c.png' } ]
   end
 end
