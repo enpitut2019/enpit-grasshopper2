2132e5d2d9b1d710128948a91282e94be94652e5
hiroto7 <hiroto450@gmail.com>
2019-08-01 15:25:40 +0900

    ダミーアイコンをランキングの表に表示

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,5 +1,13 @@
 class RankingController < ApplicationController
   def show
-    @users = [ { id: 1, name: '太郎', icon: 'a.png' }, { id: 2, name: '次郎', icon: 'b.png' }, { id: 3, name: '三郎', icon: 'c.png' } ]
+    @users = [
+      { id: 1, name: '太郎', icon: '/assets/a.jpg', score: 10000 },
+      { id: 2, name: '次郎', icon: '/assets/b.jpg', score: 9000 },
+      { id: 3, name: '三郎', icon: '/assets/c.jpg', score: 3 }
+    ]
+    @myId = 1
+    @myName = '太郎'
+    @myIcon = 'a.png'
+    @myScore = 10000
   end
 end
