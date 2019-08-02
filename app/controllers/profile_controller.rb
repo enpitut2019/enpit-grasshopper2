56d4465db6eeb3d26439364308df378a27522ab3
mori <tk011875@gmail.com>
2019-08-02 18:51:02 +0900

    added func that gets user's experience from DB

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,10 +2,16 @@ class ProfileController < ApplicationController
   def show
     @avatar_name="太郎"
     @level=1
-    @exp=100
+    @exp=get_exp(1)
     @comment="頑張る"
     @icon="assets/a.jpg"
     @icon2="assets/b.jpg"
     @count=1
+  end
+
+  def get_exp(login_user_id)
+    user_profile = Profile.find_by(user_id: login_user_id)
+    puts user_profile[:experience]
+    return user_profile[:experience]
   end
 end
