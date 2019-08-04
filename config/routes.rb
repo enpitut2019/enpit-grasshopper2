b1decebd4cd3bf5cbb14d9ca032d0b02ee7de78e
mori <tk011875@gmail.com>
2019-08-04 15:13:24 +0900

    added memo and record controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get '/home', to: 'profile#show'
   get '/rank', to: 'ranking#show'
-  get '/memos', to: 'memo#show'
-  get '/records', to: 'records#show'
+  get '/memo', to: 'memo#show'
+  get '/record', to: 'record#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'profile#show'
-
 end
