3f9c2aa6b3ebb7b446c79420bf2b6a965d61503a
Bunny63857 <ukiru77@gmail.com>
2019-08-04 16:47:07 +0900

    sessionの追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      #flash[:notice] = 'ログインできた'
+      redirect_to("/profile/show")
+      flash.now[:notice] = 'In'
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email / Password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
