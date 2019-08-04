28bc79671d0a18c44785ccd749170889c0b2c8ce
Bunny63857 <ukiru77@gmail.com>
2019-08-04 15:03:17 +0900

    パスワード用のgemを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'profile/show'
   get 'ranking/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'profile#show'
   #root 'ranking#show'
+  get 'login' =>'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
 end
