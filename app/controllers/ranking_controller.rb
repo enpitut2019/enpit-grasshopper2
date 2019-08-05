eeb362d345f8e57e7d1210bdf51039ddf1b7a82f
Bunny63857 <ukiru77@gmail.com>
2019-08-05 13:14:58 +0900

    ランキング画像の表示バグの修正

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,18 +3,18 @@ class RankingController < ApplicationController
   
   def show
     @users = [
-      { id: 1, name: '太郎', icon: '/assets/a.jpg', score: 10304 },
-      { id: 2, name: '次郎', icon: '/assets/b.jpg', score: 9070 },
-      { id: 3, name: '三郎', icon: '/assets/c.jpg', score: 8121 },
-      { id: 4, name: '七郎', icon: '/assets/a.jpg', score: 8012 },
-      { id: 5, name: '四郎', icon: '/assets/b.jpg', score: 7842 },
-      { id: 6, name: '五郎', icon: '/assets/c.jpg', score: 6731 },
-      { id: 7, name: '六郎', icon: '/assets/a.jpg', score: 6730 },
-      { id: 8, name: '八郎', icon: '/assets/b.jpg', score: 6400 },
-      { id: 9, name: '九郎', icon: '/assets/c.jpg', score: 3012 },
-      { id: 10, name: '十浪', icon: '/assets/a.jpg', score: 2057 },
-      { id: 11, name: '重一郎', icon: '/assets/b.jpg', score: 1203 },
-      { id: 12, name: '十二郎', icon: '/assets/c.jpg', score: 3 }
+      { id: 1, name: '太郎', icon: '/assets/a.png', score: 10304 },
+      { id: 2, name: '次郎', icon: '/assets/b.png', score: 9070 },
+      { id: 3, name: '三郎', icon: '/assets/c.png', score: 8121 },
+      { id: 4, name: '七郎', icon: '/assets/a.png', score: 8012 },
+      { id: 5, name: '四郎', icon: '/assets/b.png', score: 7842 },
+      { id: 6, name: '五郎', icon: '/assets/c.png', score: 6731 },
+      { id: 7, name: '六郎', icon: '/assets/a.png', score: 6730 },
+      { id: 8, name: '八郎', icon: '/assets/b.png', score: 6400 },
+      { id: 9, name: '九郎', icon: '/assets/c.png', score: 3012 },
+      { id: 10, name: '十浪', icon: '/assets/a.png', score: 2057 },
+      { id: 11, name: '重一郎', icon: '/assets/b.png', score: 1203 },
+      { id: 12, name: '十二郎', icon: '/assets/c.png', score: 3 }
     ]
     @myId = 1
     @myName = '太郎'
