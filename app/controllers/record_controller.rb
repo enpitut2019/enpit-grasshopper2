b7f3a0747cccfd0e590f5e84542a6c4923551286
katoyu <yuik2525@gmail.com>
2019-08-05 14:20:01 +0900

    記録ページの値を表示

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -2,6 +2,9 @@ class RecordController < ApplicationController
   layout 'contents'
   
   def show
-  
+    
+    @current_profile=Profile.find_by(user_id: current_user[:id])
+    @records=Record.where(user_id: current_user[:id])
+    @memos = Memo.all
   end
 end
