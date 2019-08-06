e09f954abf3e3a78b0eb31e424c7c78c6a11e9e1
hiroto7 <hiroto450@gmail.com>
2019-08-06 15:08:40 +0900

    プロフィール画面に現在経験値/レベルアップまでに必要な経験値を表示

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,30 @@
 class Profile < ApplicationRecord
+    # 自身の現在のレベル。
+    def get_level
+        level = 1;
+        while get_required_total_exp(level) < self[:experience] do
+            level += 1
+        end
+        return level
+    end
+
+    # 自身が次のレベルに上がるのに必要な累計経験値。
+    def get_next_total_exp
+        return get_required_total_exp(get_level)
+    end
+
+    private
+    # levelに上がるために必要な累計経験値。
+    def get_required_total_exp(level)
+        total_exp = 0
+        for i in 1..(level-1) do
+            total_exp += get_required_level_exp(i)
+        end
+        return total_exp
+    end
+
+    # levelからlevel+1に上がるために必要な経験値。
+    def get_required_level_exp(level)
+        return (1000 * (1.1 ** level)).to_i
+    end
 end
