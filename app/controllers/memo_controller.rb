2d6319c5be6acb27aebb2567c1535e9f1d2b9918
mori <tk011875@gmail.com>
2019-08-06 21:33:34 +0900

    メモを追加できるようにした

--- app/controllers/memo_controller.rb
+++ app/controllers/memo_controller.rb
@@ -1,16 +1,48 @@
 class MemoController < ApplicationController
   layout 'contents'
 
+  def new
+    @memo = Memo.new
+    @record_id = params[:record_id]
+  end
+
   def show
     @users = User.all
     @profiles = Profile.all
     @tags = Tag.all
     @records = Record.all.order(created_at: "DESC")
 
-    @current_profile=Profile.find_by(user_id: current_user[:id])
+    @current_profile = Profile.find_by(user_id: current_user[:id])
     @memos = Memo.all
     #@profiles = Profile.where(id: @users[:id])
+  end
 
+  def create
+    #@current_record=Record.find_by[:user_id current_user[:id]]
+    @records = Record.all
+    ## TODO: view で 自分のID一致するものを判定する処理を消し以下を適応
+    # @records = Record.where(user_id: current_user[:id])
+    @rid = params[:memos][:record_id] #(@records.length+1)
+    p "debug lgoo ------\n\n\n"
+    p params
+    p params[:memos][:record_id]
+    p "debug lgoo ------"
+    @memo = Memo.new(memo: params[:memos][:memo],like_count: 0, record_id: @rid)
+    
+    if @memo.save # => Validation
+      # Success
+      redirect_to '/record'
+      #redirect_to "/profile/#{@user.id}/edit"
+      # GET "/users/#{@user.id}" => show
+    else
+      flash[:failure] = "fail"
+      # Failure
+      render 'new'
+    end
+  end
+
+  def memo_params
+    params.require(:memo).permit(:memo)
   end
 end
 
